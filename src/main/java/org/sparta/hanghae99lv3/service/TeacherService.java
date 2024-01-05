4255cc31f9f1083cfd790169f52b7df1b20df3a2
KimDabomi <cdabomi@nate.com>
2024-01-05 22:17:47 +0900

    feat: 강의 조회, 삭제, 수정

--- src/main/java/org/sparta/hanghae99lv3/service/TeacherService.java
+++ src/main/java/org/sparta/hanghae99lv3/service/TeacherService.java
@@ -5,6 +5,7 @@ import org.sparta.hanghae99lv3.dto.TeacherResponseDto;
 import org.sparta.hanghae99lv3.entity.Teacher;
 import org.sparta.hanghae99lv3.repository.TeacherRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TeacherService {
@@ -20,4 +21,28 @@ public class TeacherService {
         return new TeacherResponseDto(saveTeacher);
     }
 
+    public Teacher getTeacher(Long id, TeacherRequestDto requestDto) {
+        return findTeacher(id);
+    }
+
+    @Transactional
+    public Teacher updateTeacher(Long id, TeacherRequestDto requestDto) {
+        Teacher teacher = findTeacher(id);
+        teacher.update(requestDto);
+
+        return teacher;
+    }
+
+    public Long deleteTeacher(Long id) {
+        Teacher teacher = findTeacher(id);
+        teacherRepository.delete(teacher);
+
+        return id;
+    }
+
+    private Teacher findTeacher(Long id) {
+        return teacherRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 강사는 존재하지 않습니다.")
+        );
+    }
 }
