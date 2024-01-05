710e2be6e5049e5a46871af839b5046b5f7ab92d
KimDabomi <cdabomi@nate.com>
2024-01-05 22:03:04 +0900

    refactor: if~else문 수정

--- src/main/java/org/sparta/hanghae99lv3/filter/AuthFilter.java
+++ src/main/java/org/sparta/hanghae99lv3/filter/AuthFilter.java
@@ -33,33 +33,32 @@ public class AuthFilter implements Filter {
 
         if (StringUtils.hasText(url) && (url.startsWith("/api/login") || url.startsWith("/api/join"))) {
             chain.doFilter(request, response);
-        } else {
-            String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
-
-            if (StringUtils.hasText(tokenValue)) {
-                String token = jwtUtil.substringToken(tokenValue);
-
-                if (!jwtUtil.validateToken(token)) {
-                    throw new IllegalArgumentException("토큰을 찾을 수 없습니다.");
-                }
-
-                Claims info = jwtUtil.getUserInfoFromToken(token);
-
-                Staff staff = staffRepository.findByEmail(info.getSubject());
-                if (staff.getId() == null) {
-                    throw new IllegalArgumentException("존재하지 않는 회원입니다.");
-                }
-
-                if (!StaffAuthEnum.ADMIN.equals(staff.getAuth())) {
-                    throw new IllegalArgumentException("권한이 없습니다.");
-                }
-
-                request.setAttribute("staff", staff);
-                chain.doFilter(request, response);
-            } else {
-                throw new IllegalArgumentException("토큰을 찾을 수 없습니다.");
-            }
         }
-    }
 
+        String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
+
+        if (!StringUtils.hasText(tokenValue)) {
+            throw new IllegalArgumentException("토큰을 찾을 수 없습니다.");
+        }
+
+        String token = jwtUtil.substringToken(tokenValue);
+
+        if (!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("토큰을 찾을 수 없습니다.");
+        }
+
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+
+        Staff staff = staffRepository.findByEmail(info.getSubject());
+        if (staff.getId() == null) {
+            throw new IllegalArgumentException("존재하지 않는 회원입니다.");
+        }
+
+        if (!StaffAuthEnum.ADMIN.equals(staff.getAuth())) {
+            throw new IllegalArgumentException("권한이 없습니다.");
+        }
+
+        request.setAttribute("staff", staff);
+        chain.doFilter(request, response);
+    }
 }
