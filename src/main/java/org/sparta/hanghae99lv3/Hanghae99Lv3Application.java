92977bb4c4cd3f79f9c4ff0801e6d4bf657632d5
KimDabomi <cdabomi@nate.com>
2024-01-05 11:21:32 +0900

    docs: Initial commit

---
+++ src/main/java/org/sparta/hanghae99lv3/Hanghae99Lv3Application.java
@@ -0,0 +1,13 @@
+package org.sparta.hanghae99lv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99Lv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanghae99Lv3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/hanghae99lv3/Hanghae99Lv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.hanghae99lv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghae99Lv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
